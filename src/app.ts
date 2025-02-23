c1e586c248cd5fc28b82b0bd61c5b49404e38f9a
SwiichyCode <adlpromail@gmail.com>
2025-02-23 19:50:14 +0100

    feat: add test routes to application

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { limiter } from './middlewares/rate-limiter';
 import { errorHandler } from './middlewares/error-handler';
 import chatbotRoutes from './routes/chatbot.route';
+import testRoutes from './routes/test.route';
 
 const app = express();
 app.set('trust proxy', 1);
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(limiter);
 app.use('/api/chatbot', chatbotRoutes);
+app.use('/api/test', testRoutes);
 app.use(errorHandler);
 
 export default app;
